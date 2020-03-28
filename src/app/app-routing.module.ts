374dc4908e77d8bf404272eb59347013bf8deadb
vedant <vedantkarpe1@gmail.com>
2020-03-28 23:29:19 +0530

    updated code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './modules/feature/home/home/home.component';
 
 const routes: Routes =
 [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'product', loadChildren: () => import('./modules/feature/product/product.module').then(m => m.ProductModule) },
   { path: 'checkout', loadChildren: () => import('./modules/feature/checkout/checkout.module').then(m => m.CheckoutModule) },
-  { path: 'admin', loadChildren: () => import('./modules/feature/admin/admin.module').then(m => m.AdminModule) }
+  { path: 'admin', loadChildren: () => import('./modules/feature/admin/admin.module').then(m => m.AdminModule)},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
